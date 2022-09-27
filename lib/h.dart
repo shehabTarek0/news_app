d4bd23d22da08fa583e88d5d83f4ea5c7f72a498
Shehab_tarek <shehabt414@gmail.com>
2022-09-27 04:01:32 +0200

    q

---
+++ lib/h.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Hscreen extends StatelessWidget {
+  const Hscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
